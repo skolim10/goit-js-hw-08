34a93240e164d1195646c6069e31474f4a4012e3
Tomasz Skolimo <skolim10@gmail.com>
2022-08-12 19:52:47 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const text = document.querySelector('textarea');
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    const input = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(input));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+
+  event.currentTarget.reset();
+  localStorage.clear();
+});
+
+const storage = localStorage.getItem('feedback-form-state');
+const parseStorageData = JSON.parse(storage);
+const tryFoo = () => {
+  if (parseStorageData !== null) {
+    email.value = parseStorageData.email;
+    text.value = parseStorageData.message;
+  }
+};
+
+tryFoo();
