73ec37150e0c0197408d8e536ae3fe237300a444
Tomasz Skolimo <skolim10@gmail.com>
2022-08-12 18:56:39 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ duration, percent, seconds }) => {
+    localStorage.setItem('videoplayer-current-time', `${seconds}`);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
